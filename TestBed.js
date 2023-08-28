6e9e1faea0fa51384d78d09150cacea8216c7302
Gavin Trotman-Pitt <gtpitt@users.noreply.github.com>
2023-08-28 09:18:23 -0400

    Update TestBed.js

--- TestBed.js
+++ TestBed.js
@@ -1,11 +1,8 @@
 
-      function load() {
+      function tbload() {
          setTimeout(myURL, 9000);
          var result = document.getElementById("result");
          result.innerHTML = `The page will load after delay of 5 seconds using setTimeout()  method.`;
       }
 
-      function myURL() {
-         window.open('https://www.tutorialspoint.com/index.htm', name = self);
-      }
 
