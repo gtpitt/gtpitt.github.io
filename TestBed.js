a5dff2a91b1bbc4b7b131bd184bb9de1772925db
Gavin Trotman-Pitt <gtpitt@users.noreply.github.com>
2023-08-28 10:09:57 -0400

    Update TestBed.js

--- TestBed.js
+++ TestBed.js
@@ -5,7 +5,7 @@
       }
 function tbresult() {
           var result = document.getElementById("result");
-         result.innerHTML = `The page will load after delay of 5 seconds using setTimeout()  method.`;
+         result.innerHTML = 'The page will load after delay of 5 seconds using setTimeout()  method.';
 }
 
 
