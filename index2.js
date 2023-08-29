cc47d5cf697b7f6a68fdf7593789333500fa4c5d
Gavin Trotman-Pitt <gtpitt@users.noreply.github.com>
2023-08-29 11:07:46 -0400

    Add files via upload

--- index2.js
+++ index2.js
@@ -8,6 +8,7 @@ function iresult() {
 	var result = document.getElementById("result");
 	result.innerHTML = 'The page will load after delay of 5 seconds using setTimeout()  method.';
 }
+
 $(document).ready(function () {
 	event.preventDefault();
 
